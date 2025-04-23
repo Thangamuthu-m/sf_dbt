a96b85445cd9d66bbdd4e287c10dd17651b44cb4
Thangamuthu M <m.thangamuthu@yahoo.co.in>
2025-04-23 13:26:37 +0000

    added one more transformation dept_wise_emp_cnt

--- models/STAGE/stage_employee_history.sql
+++ models/STAGE/stage_employee_history.sql
@@ -4,7 +4,10 @@
     )
 }}
 
+{% set start_date = '1993-01-01' %}   -- Variable declaration
+{% set end_date = '1997-08-31' %}     -- Variable declaration
+
   select job_title,department_name,concat(e.first_name,'',e.last_name) as fullname
    from {{ ref('raw_employee') }} e inner join {{ ref('raw_job_history') }} jh on e.employee_id = jh.employee_id
-   and jh.start_date between '1993-01-01' and '1997-08-31' inner join {{ ref('raw_jobs') }} j on j.job_id=jh.job_id
+   and jh.start_date between '{{start_date}}' and '{{end_date}}' inner join {{ ref('raw_jobs') }} j on j.job_id=jh.job_id
    inner join {{ ref('raw_department') }} d on d.department_id = jh.department_id
